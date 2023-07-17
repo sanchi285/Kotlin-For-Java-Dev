03910ede24e08c75a304f3434597e70eb3806420
sanchi285 <bismoy.sanchi@gmail.com>
2023-07-17 23:34:26 +0600

    === added

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -6,11 +6,15 @@ fun main(args: Array<String>) {
     val  employee2 = Employee("John", 2)
     val  employee3 = Employee("John", 2)
 
+    val employee4 = employee2
+
     println(employee1==employee2)
-    println(employee2==employee3)
+    println(employee2===employee3)
     println(employee3.equals(employee2))
     println(employee2.equals(employee1))
-
+    println(employee2==employee4)
+    employee4.name="Karim"
+    println(employee2.name)
 }
 
 class Employee(var name: String, val id: Int){
