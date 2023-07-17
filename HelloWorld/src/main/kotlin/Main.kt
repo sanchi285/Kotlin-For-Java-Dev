1ad5f86a9933846955f9cf669e79f47a92452e47
sanchi285 <bismoy.sanchi@gmail.com>
2023-07-17 23:31:04 +0600

    Kotlin equality

--- HelloWorld/src/main/kotlin/Main.kt
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -1,4 +1,23 @@
 fun main(args: Array<String>) {
     val number: Int
     number = 10
+
+    val employee1 = Employee("Mary", 1)
+    val  employee2 = Employee("John", 2)
+    val  employee3 = Employee("John", 2)
+
+    println(employee1==employee2)
+    println(employee2==employee3)
+    println(employee3.equals(employee2))
+    println(employee2.equals(employee1))
+
+}
+
+class Employee(var name: String, val id: Int){
+    override fun equals(obj: Any?): Boolean {
+       if (obj is Employee){
+           return name==obj.name && id == obj.id;
+       }
+        return false;
+    }
 }
