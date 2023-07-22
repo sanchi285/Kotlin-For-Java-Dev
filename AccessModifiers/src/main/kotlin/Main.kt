2359e2fbd27aa84cb7b2f40eb2e2249d72f1cf05
sanchi285 <bismoy.sanchi@gmail.com>
2023-07-22 22:03:22 +0600

    Basic Kotlin class has been added

--- AccessModifiers/src/main/kotlin/Main.kt
+++ AccessModifiers/src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
-    val emp = Employee()
-    println(emp)
+    val emp = Employee("Rabiul Bhai Expert")
+    println(emp.firstName)
+    val emp2  = Employee("joe",false);
+
+    println(emp2.firstName)
+    println(emp2.fulltime)
 }
 
-private class Employee {
-
+class Employee (val firstName: String) {
+    var fulltime: Boolean = true
+    constructor(firstName: String, fulltime: Boolean) :this (firstName){
+        this.fulltime = fulltime
+    }
 }
