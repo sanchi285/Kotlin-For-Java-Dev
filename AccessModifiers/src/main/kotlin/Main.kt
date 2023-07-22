3436c4d828bc4b55722db75a0f79ee2900b24d73
sanchi285 <bismoy.sanchi@gmail.com>
2023-07-22 22:25:56 +0600

    Getter setter added for fields

--- AccessModifiers/src/main/kotlin/Main.kt
+++ AccessModifiers/src/main/kotlin/Main.kt
@@ -4,15 +4,52 @@ fun main(args: Array<String>) {
     println("Hello World!")
     val emp = Employee("Rabiul Bhai Expert")
     println(emp.firstName)
-    val emp2  = Employee("joe",false);
+    val emp2  = Employee("joe",false,90);
 
     println(emp2.firstName)
-    println(emp2.fulltime)
+    println(emp2.fullTime)
+
+    val dmc = Demo()
+    println(dmc.dummy)
+    emp2.fullTime=true
+
+    emp2.salary=890;
+    println(emp2.salary)
 }
 
-class Employee (val firstName: String) {
-    var fulltime: Boolean = true
-    constructor(firstName: String, fulltime: Boolean) :this (firstName){
-        this.fulltime = fulltime
+//primary constructor example
+class Employee (val firstName: String, fullTime: Boolean = true, salary: Int=0) {
+    var fullTime =  fullTime
+        get(){
+            println("custome test emp")
+            return field
+        }
+
+        set(value){
+            println("custome test set")
+            field = value
+        }
+    var salary = salary
+        get(){
+            println("custome get sal")
+            return field
+        }
+
+        set(value){
+            println("custome  set sal")
+            field = value
+        }
+
+}
+
+//Secondary Constructor
+class Demo{
+    val dummy: String
+    constructor(dummy: String){
+        this.dummy = dummy
+    }
+
+    constructor(){
+        this.dummy = "Hello"
     }
 }
